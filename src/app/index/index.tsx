6bb929e99173361022248f3fa09362f455069bfc
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-12-29 19:30:30 -0300

    class:Criando o Header da Index

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,11 +1,18 @@
-import { Text, View } from 'react-native'
+import { Image, TouchableOpacity, View } from 'react-native'
 import styles from './styles'
+import { MaterialIcons } from '@expo/vector-icons'
+import { colors } from '@/styles/colors'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <Text style={styles.title}>React Native!</Text>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity>
+          <MaterialIcons name='add' size={32} color={colors.green[300]}/>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -1,16 +1,28 @@
 import { colors } from "@/styles/colors";
 import { StyleSheet } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    paddingTop: 62,
   },
   title: {
     color: colors.green[900],
     fontSize: 22
-  }
+  },
+  header: {
+    paddingHorizontal: 24,
+    width: "100%",
+    flexDirection: "row", 
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  logo: {
+    width: 32,
+    height: 38,
+  },
 })
 
 export default styles;
