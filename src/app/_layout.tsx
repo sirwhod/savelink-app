a165dee58cc110d997a55034089234054a005a6c
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-12-29 19:18:08 -0300

    class: Definindo a Cor de Fundo do App

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { colors } from "@/styles/colors";
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  const backgroundColor = colors.gray[950];
+
+  return <Stack
+    screenOptions={{
+      headerShown: false,
+      contentStyle: { backgroundColor }
+    }}
+  />
+}
