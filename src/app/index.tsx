f65208cbd8ad59f262e0a3e978d9e87ce47b92f6
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-12-29 18:57:57 -0300

    class: Executando o App

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello React Native App</Text>
+      <Text>Meu App</Text>
+    </View>
+  );
+}
