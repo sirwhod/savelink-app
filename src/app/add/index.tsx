5689533fb4ed4b66390cfac8de67fd296cbbef45
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-01-04 16:04:30 -0300

    class: Criando a Tela para Adicionar Novos Links

---
+++ src/app/add/index.tsx
@@ -0,0 +1,21 @@
+import { colors } from "@/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Add() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Novo</Text>
+      </View>
+
+
+      <Text style={styles.label}>Selecione uma categoria</Text>
+    </View>
+  )
+}
