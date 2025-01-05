431a201858b47c5221075052883061c718200b71
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-01-05 15:58:02 -0300

    class: Métodos de Listar e Salvar

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,34 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+const LINKS_STORE_KEY = "savelink-app@links"
+
+type LinkStorage = {
+  id: string
+  name: string
+  url: string	
+  category: string
+}
+
+async function get(): Promise<LinkStorage[]> {
+  try {
+    const storage = await AsyncStorage.getItem(LINKS_STORE_KEY)
+    const response = storage ? JSON.parse(storage) : []
+
+    return response
+  } catch (error) {
+    throw error
+  }
+}
+
+async function save(newLink: LinkStorage) {
+  try {
+    const storage = await get()
+    const updatedLinks = JSON.stringify([...storage, newLink])
+
+    await AsyncStorage.setItem(LINKS_STORE_KEY, updatedLinks)
+  } catch (error) {
+    throw error
+  }
+}
+
+export const linkStorage = { get, save }
